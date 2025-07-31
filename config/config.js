00309d78e0a1c5d85d736ed554820c6f924447d9
changhyunha <hacwx1@gmail.com>
2025-07-31 11:02:33 +0900

    소스 병합

--- config/config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-//const API_BASE_URL = 'http://172.30.1.31:80';
-//const API_BASE_URL = 'http://172.30.1.31:3001';
-//const API_BASE_URL = 'https://eb.awssaasumhaha.xyz';
-const API_BASE_URL = 'https://ptbbqct2jib4r2bwozaizyn6bq0luctk.lambda-url.ap-northeast-2.on.aws';
-
-
-export { API_BASE_URL };
